b90b638da8e71c912af5f8a2543eacbd8c5d8c41
Lina <lsrodriguez@unillanos.edu.co>
2026-01-13 02:58:52 -0500

    test:add controller tests for rest controllers

--- src/test/java/com/franchise/api/FranchiseApiApplicationTests.java
+++ src/test/java/com/franchise/api/FranchiseApiApplicationTests.java
@@ -1,13 +1,8 @@
 package com.franchise.api;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class FranchiseApiApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
