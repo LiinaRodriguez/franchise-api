f75275aa019e87ffa778ad10044bedae29c80bda
Lina <lsrodriguez@unillanos.edu.co>
2026-01-12 20:06:23 -0500

    feat(infra): add entities

---
+++ src/main/java/com/franchise/api/infra/adapter/persistence/entity/FranchiseEntity.java
@@ -0,0 +1,29 @@
+package com.franchise.api.infra.adapter.persistence.entity;
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+
+@Getter
+@Builder(toBuilder = true)
+@AllArgsConstructor
+@Table("franchises")
+public class FranchiseEntity {
+
+    @Id
+    private Long id;
+
+    private String name;
+
+    @Column("created_at")
+    private LocalDateTime createdAt;
+
+    @Column("updated_at")
+    private LocalDateTime updatedAt;
+}
